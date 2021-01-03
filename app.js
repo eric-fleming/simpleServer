19ad06dcfe7e42221e11f6dd4b0ce2c78e0e5128
Eric Fleming <ericfleming8@gmail.com>
2021-01-02 21:08:01 -0600

    feat: serves index.html from the public folder

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
+// middleware plugin to grab static content from the public folder
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+// default route
 app.get('/', (req, res) => {
+  res.sendFile('./public/index/index.html', { root: __dirname });
+});
+
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
