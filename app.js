f1a9a7cf561d5087f3a63112220ca5a2cc2550fa
Eric Fleming <ericfleming8@gmail.com>
2021-01-02 22:04:55 -0600

    feat: app can call upon files & scripts from src folder

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 // middleware plugin to grab static content from the public folder
-app.use('/static', express.static(path.join(__dirname, 'src')))
+app.use(express.static('src'));
 
 // default route
 app.get('/', (req, res) => {
