b62d15d29bfc1ac821cfb45ed4e37e00c11d511f
Eric Fleming <ericfleming8@gmail.com>
2021-01-05 19:10:56 -0600

    fix: port set to default or 8080

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080;
 
 // middleware plugin to grab static content from the public folder
 app.use(express.static('src'));
